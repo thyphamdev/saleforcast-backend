13fd56322db51d029f695522022131549cde09a3
thyphamdev <landang@Lans-MacBook-Pro.local>
2023-04-16 17:11:18 +0200

    feat: add cors for localhost

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:3000' }); // only for now, should be in env var later on
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
